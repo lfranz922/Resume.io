f608cadf4fac11d6a044f49f72d9809e49e5bbdc
Lukas Franz <lfranz922@gmail.com>
2023-08-29 23:51:22 -0700

    cloned working

---
+++ README.txt
@@ -0,0 +1,5 @@
+this is my personal website
+
+i'm going to try to maintain it as best as possible
+
+web design is not my passion but I try my best
--- assets/js/main.js
+++ assets/js/main.js
@@ -210,3 +210,9 @@
 		}
 
 })(jQuery);
+
+// When the user clicks on div, open the popup
+function myFunction() {
+	var popup = document.getElementById("myPopup");
+	popup.classList.toggle("show");
+  }
